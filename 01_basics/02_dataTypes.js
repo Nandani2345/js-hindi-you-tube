d2ee3aee4dcf5e7c122d11f6a298563db0e21e25
Nanbackend <132212641+Nandani2345@users.noreply.github.com>
2024-08-06 13:25:41 +0000

    dataypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //traet all JS code as newer version
+ 
+// alert( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +3
+) // code readability should be high
+
+console.log("nandani gehlot")
+
+let name = "nandani" //string value
+let age = 18 // number value
+let isLoggedIn = true //boolean type
+
+//premitive data type->
+
+//number => 2 th power 53
+//bigInt
+//String use single qote or double quote
+//boolean => true/false
+//null => standalone value/representation of empty value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
